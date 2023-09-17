9f927dec8941e8081f5679deac6508fd086f289a
Nico <nlovejoy@ondema.io>
2023-09-17 11:27:08 -0700

    first commit

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+/**
+ 
+Main()
+    Load File - request to update records
+    Validate Publisher
+    Validate CSV format
+    See if records need to be inserted
+    See if records need to be updated
+    report back in some fashion
+  
+ */
